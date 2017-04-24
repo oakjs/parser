27ed74583e9866017f67a669cc5b5882301efcfa
owenoak <owenoak@gmail.com>
2017-04-24 16:42:32 -0400

    numbers tests

---
+++ src/rules/numbers.test.js
@@ -0,0 +1,43 @@
+import Rule from "../RuleSyntax";
+import "./core.js";
+import parser from "./numbers.js";
+
+test("parser is defined", () => {
+	expect(parser).toBeDefined();
+});
+
+test("index expression with #", () => {
+	let result = parser.parse("expression", "item #1 of myList");
+	expect(result.toSource()).toBe("spell.getItem(myList, 1)");
+});
+
+test("negative index expression with #", () => {
+	let result = parser.parse("expression", "item #-1 of myList");
+	expect(result.toSource()).toBe("spell.getItem(myList, -1)");
+});
+
+test("index expression without #", () => {
+	let result = parser.parse("expression", "item 1 of myList");
+	expect(result.toSource()).toBe("spell.getItem(myList, 1)");
+});
+
+test("negative index expression without #", () => {
+	let result = parser.parse("expression", "item -11 of myList");
+	expect(result.toSource()).toBe("spell.getItem(myList, -11)");
+});
+
+test("index expression with custom identifier", () => {
+	let result = parser.parse("expression", "word -11 of myList");
+	expect(result.toSource()).toBe("spell.getItem(myList, -11)");
+});
+
+test("ordinal index expression", () => {
+	let result = parser.parse("expression", "the first item of myList");
+	expect(result.toSource()).toBe("spell.getItem(myList, 1)");
+});
+
+test("ordinal index expression with custom identifier", () => {
+	let result = parser.parse("expression", "the first word of myList");
+	expect(result.toSource()).toBe("spell.getItem(myList, 1)");
+});
+
