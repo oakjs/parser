2da563e2cea88adedc58b8c136e7b6e27a4a4372
Owen Williams <owenoak@gmail.com>
2019-02-13 17:47:25 -0800

    `rule.stack` to `{ rules, starts }`

--- src/Parser.js
+++ src/Parser.js
@@ -10,6 +10,7 @@ import { cloneClass } from "./utils/class.js";
 
 // GRRR... will SOMEONE on the node team please implement console.group ???
 if (!console.group) console.group = console.log;
+if (!console.groupCollapsed) console.groupCollapsed = console.group;
 if (!console.groupEnd) console.groupEnd = console.log;
 
 // Error we'll throw for problems when parsing.
