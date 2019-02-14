f36d2d0d5ad39004b32f726d66d9ef89363b4135
Owen Williams <owenoak@gmail.com>
2019-02-14 03:47:27 -0800

    ParseError working better in browser

--- src/ParseError.js
+++ src/ParseError.js
@@ -1,7 +1,10 @@
 // Error we'll throw for problems when parsing.
 // Uses a specific type so we can check for it in tests.
-export default function ParseError(...args) {
-  Error.apply(this, args);
-  if (Error.captureStackTrace) Error.captureStackTrace(this, ParseError);
+
+export default class ParseError extends Error {
+  constructor(...props) {
+    super(...props);
+    // Fix up stack trace to refer to throwing line
+    if (Error.captureStackTrace) Error.captureStackTrace(this, this.constructor);
+  }
 }
-ParseError.prototype = new Error();
