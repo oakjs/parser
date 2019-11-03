d0770052470c07b5bc3f880f801153597b80106a
Owen Williams <owenoak@gmail.com>
2019-11-03 12:51:45 -0800

    `yarn prettier` including jsx

--- src/app/SpellEditor.jsx
+++ src/app/SpellEditor.jsx
@@ -1,31 +1,31 @@
-import React from "react";
+import React from "react"
 
-import Card from "react-bootstrap/Card";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import NavLink from "react-bootstrap/NavLink";
+import Card from "react-bootstrap/Card"
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import Button from "react-bootstrap/Button"
+import Nav from "react-bootstrap/Nav"
+import Navbar from "react-bootstrap/Navbar"
+import NavDropdown from "react-bootstrap/NavDropdown"
+import NavLink from "react-bootstrap/NavLink"
 
-import Octicon, {ChevronRight} from '@githubprimer/octicons-react'
+import Octicon, { ChevronRight } from "@githubprimer/octicons-react"
 
-import { projects as _projects, withProjects, INPUT } from "./redux/projects";
+import { projects as _projects, withProjects, INPUT } from "./redux/projects"
 
 // Codemirror
-import { Controlled as CodeMirror } from 'react-codemirror2'
-import 'codemirror/lib/codemirror.css';
-import 'codemirror/theme/neo.css';
-import 'codemirror/theme/neat.css';
-import 'codemirror/theme/solarized.css';
-import 'codemirror/mode/javascript/javascript';
-import 'codemirror/mode/markdown/markdown';
-import './spell.codemirror.js'
-import './SpellEditor.css';
+import { Controlled as CodeMirror } from "react-codemirror2"
+import "codemirror/lib/codemirror.css"
+import "codemirror/theme/neo.css"
+import "codemirror/theme/neat.css"
+import "codemirror/theme/solarized.css"
+import "codemirror/mode/javascript/javascript"
+import "codemirror/mode/markdown/markdown"
+import "./spell.codemirror.js"
+import "./SpellEditor.css"
 // load and configure JSHINT global variable (ugh, this is nasty)
-import { JSHINT } from 'jshint'
+import { JSHINT } from "jshint"
 window.JSHINT = function(source) {
   // see: https://jshint.com/docs/options
   const hintOptions = {
@@ -37,65 +37,67 @@ window.JSHINT = function(source) {
   }
   return JSHINT(source, hintOptions)
 }
-Object.keys(JSHINT).forEach(key => window.JSHINT[key] = JSHINT[key])
-import 'codemirror/addon/lint/lint.js'
-import 'codemirror/addon/lint/lint.css'
-import 'codemirror/addon/lint/javascript-lint.js'
-
+Object.keys(JSHINT).forEach(key => (window.JSHINT[key] = JSHINT[key]))
+import "codemirror/addon/lint/lint.js"
+import "codemirror/addon/lint/lint.css"
+import "codemirror/addon/lint/javascript-lint.js"
 
 export class _SpellEditor extends React.Component {
   constructor(props) {
-    super(props);
-    _projects.call.startup();
+    super(props)
+    _projects.call.startup()
   }
 
-  compile = () => _projects.call.compileInput();
-  revert = () => _projects.call.revertInput();
-  reload = () => _projects.call.reloadSelected();
-  save = () => _projects.call.saveInput();
-  selectProject = (projectId) => _projects.call.selectModule({ projectId });
-  selectModule = (moduleId) => _projects.call.selectModule({ projectId: this.props.projectId, moduleId });
+  compile = () => _projects.call.compileInput()
+  revert = () => _projects.call.revertInput()
+  reload = () => _projects.call.reloadSelected()
+  save = () => _projects.call.saveInput()
+  selectProject = projectId => _projects.call.selectModule({ projectId })
+  selectModule = moduleId => _projects.call.selectModule({ projectId: this.props.projectId, moduleId })
 
   create = () => {
-    const moduleId = prompt("Name for the new module?", "Untitled");
-    if (!moduleId) return;
-    const contents = `## Module ${moduleId}`;
-    _projects.call.newModule({ moduleId, contents });
+    const moduleId = prompt("Name for the new module?", "Untitled")
+    if (!moduleId) return
+    const contents = `## Module ${moduleId}`
+    _projects.call.newModule({ moduleId, contents })
   }
 
   duplicate = () => {
-    const { moduleId } = this.props.projects;
-    const newModuleId = prompt("Name for the new module?", moduleId);
-    if (!newModuleId || newModuleId === moduleId) return;
-    _projects.call.duplicateModule({ moduleId, newModuleId, contents: INPUT });
+    const { moduleId } = this.props.projects
+    const newModuleId = prompt("Name for the new module?", moduleId)
+    if (!newModuleId || newModuleId === moduleId) return
+    _projects.call.duplicateModule({ moduleId, newModuleId, contents: INPUT })
   }
 
   _delete = () => {
-    const { moduleId } = this.props.projects;
-    if (!confirm(`Really delete '${moduleId}'?`)) return;
-    _projects.call.deleteModule({ moduleId });
+    const { moduleId } = this.props.projects
+    if (!confirm(`Really delete '${moduleId}'?`)) return
+    _projects.call.deleteModule({ moduleId })
   }
 
   rename = () => {
-    const { moduleId } = this.props.projects;
-    const newModuleId = prompt("New name?", moduleId);
-    if (!newModuleId || newModuleId === moduleId) return;
-    _projects.call.renameModule({ moduleId, newModuleId, contents: INPUT });
+    const { moduleId } = this.props.projects
+    const newModuleId = prompt("New name?", moduleId)
+    if (!newModuleId || newModuleId === moduleId) return
+    _projects.call.renameModule({ moduleId, newModuleId, contents: INPUT })
   }
 
   onInputChange = (codeMirror, change, value) => {
     //console.info(codeMirror, "\n", change)//, "\n", value)
-    _projects.actions.updateInput(value);
+    _projects.actions.updateInput(value)
   }
 
   // Buttons to show when the input field is dirty.
   dirtyButtons = (
     <div style={{ position: "absolute", right: "3px", top: "3px" }}>
-      <Button variant="danger" onClick={this.revert} style={{ width: "5em" }}><u>R</u>evert</Button>
-      {" "}
-      <Button variant="success" onClick={this.save} style={{ width: "5em" }}><u>S</u>ave</Button>
+      <Button variant="danger" onClick={this.revert} style={{ width: "5em" }}>
+        <u>R</u>evert
+      </Button>{" "}
+      <Button variant="success" onClick={this.save} style={{ width: "5em" }}>
+        <u>S</u>ave
+      </Button>
     </div>
-   );
+  )
 
   // Buttons to show when the input can be compiled.
   compileButton = (
@@ -110,38 +112,42 @@ export class _SpellEditor extends React.Component {
       }}
       onClick={this.compile}
     >
-      <Octicon icon={ChevronRight} size="medium"/>
+      <Octicon icon={ChevronRight} size="medium" />
     </Button>
-  );
+  )
 
   render() {
-    const { projects } = this.props;
-    const { projectId, moduleId, input, output, dirty } = projects;
-    const projectIds = _projects.getProjectIds(projects);
-    const index = _projects.getProjectIndex(projects, projectId);
+    const { projects } = this.props
+    const { projectId, moduleId, input, output, dirty } = projects
+    const projectIds = _projects.getProjectIds(projects)
+    const index = _projects.getProjectIndex(projects, projectId)
 
     // Don't do anything if our data isn't yet loaded
-    if (!projectIds || !index) return null;
+    if (!projectIds || !index) return null
 
     // Create menuItems for the projects menu
-    const projectItems = projectIds.map( projectId =>
-      <NavDropdown.Item key={projectId} eventKey={projectId} onSelect={this.selectProject}>{projectId}</NavDropdown.Item>
-    );
+    const projectItems = projectIds.map(projectId => (
+      <NavDropdown.Item key={projectId} eventKey={projectId} onSelect={this.selectProject}>
+        {projectId}
+      </NavDropdown.Item>
+    ))
 
     // Create menuitems for the modules menu
-    const moduleItems = index.modules.map( module =>
-      <NavDropdown.Item key={module.id} eventKey={module.id} onSelect={this.selectModule}>{module.id}</NavDropdown.Item>
-    );
+    const moduleItems = index.modules.map(module => (
+      <NavDropdown.Item key={module.id} eventKey={module.id} onSelect={this.selectModule}>
+        {module.id}
+      </NavDropdown.Item>
+    ))
 
-    const theme = "neat"; // Owen favors: "solarized", "neo" and "neat"
+    const theme = "neat" // Owen favors: "solarized", "neo" and "neat"
     const codeMirrorOptions = {
       theme: "neat", // Owen favors: "solarized", "neo" and "neat"
       indentUnit: 3,
-      tabSize: 3,
+      tabSize: 3
     }
     const inputOptions = {
       ...codeMirrorOptions,
-      mode: "spell",
+      mode: "spell"
     }
 
     const outputOptions = {
@@ -160,17 +166,21 @@ export class _SpellEditor extends React.Component {
             <Navbar bg="dark" variant="dark" className="py-0">
               <Nav>
                 <NavLink disabled>Project:</NavLink>
-                <NavDropdown title={projectId} id="project-dropdown" style={{width: "12em"}}>
+                <NavDropdown title={projectId} id="project-dropdown" style={{ width: "12em" }}>
                   {projectItems}
                 </NavDropdown>
                 <NavLink disabled>File:</NavLink>
-                <NavDropdown title={moduleId} id="module-dropdown" style={{width: "12em"}}>
+                <NavDropdown title={moduleId} id="module-dropdown" style={{ width: "12em" }}>
                   {moduleItems}
                 </NavDropdown>
                 <Navbar.Collapse id="navbar-buttons" className="ml-2">
                   <Nav>
-                    <Button variant={dirty ? "success" : "dark"} onClick={this.save}>Save</Button>
-                    <Button variant={dirty ? "danger" : "dark"} onClick={this.revert}>Revert</Button>
+                    <Button variant={dirty ? "success" : "dark"} onClick={this.save}>
+                      Save
+                    </Button>
+                    <Button variant={dirty ? "danger" : "dark"} onClick={this.revert}>
+                      Revert
+                    </Button>
                     <Nav.Link onClick={this.create}>New File</Nav.Link>
                     <Nav.Link onClick={this.duplicate}>Duplicate</Nav.Link>
                     <Nav.Link onClick={this.rename}>Rename</Nav.Link>
@@ -201,10 +211,10 @@ export class _SpellEditor extends React.Component {
           {output ? null : this.compileButton}
         </Row>
       </Container>
-    );
+    )
   }
 }
 
 // Pass projects from redux
-const SpellEditor = withProjects(_SpellEditor);
+const SpellEditor = withProjects(_SpellEditor)
 export { SpellEditor }
--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,22 +1,22 @@
 // Common imports
-import React from "react";
-import ReactDOM from 'react-dom';
-import { Provider } from "react-redux";
+import React from "react"
+import ReactDOM from "react-dom"
+import { Provider } from "react-redux"
 
-require("./debug.js");
+require("./debug.js")
 
-const { store } = require("./all.js");
-const { SpellEditor } = require("./SpellEditor.jsx");
+const { store } = require("./all.js")
+const { SpellEditor } = require("./SpellEditor.jsx")
 
 function renderApp() {
   ReactDOM.render(
     <Provider store={store}>
       <SpellEditor />
     </Provider>,
-    document.getElementById('react-root')
-  );
-};
+    document.getElementById("react-root")
+  )
+}
 
-renderApp();
+renderApp()
 
 //module.hot.accept(renderApp);
