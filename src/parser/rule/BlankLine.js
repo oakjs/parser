8e519cc416c8b8a88ac132d9d30969fcb9ff26b8
Owen Williams <owenoak@gmail.com>
2020-04-11 09:28:14 -0700

    BlankLine.compile()

--- src/parser/rule/BlankLine.js
+++ src/parser/rule/BlankLine.js
@@ -1,4 +1,8 @@
 import Rule from "./Rule"
 
 // Blank line representation in parser output.
-Rule.BlankLine = class blank_line extends Rule {}
+Rule.BlankLine = class blank_line extends Rule {
+  compile(match) {
+    return "\n"
+  }
+}
