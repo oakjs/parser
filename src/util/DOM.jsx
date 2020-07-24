4ea6c324061a6eb03d7c06daebe6287ad6931fb9
Owen Williams <owenoak@gmail.com>
2020-07-23 21:29:42 -0700

    DOM: getComputedStyle(), getPadding(), getMargins(), getBorder()

--- src/util/DOM.jsx
+++ src/util/DOM.jsx
@@ -1,4 +1,5 @@
 /** Browser DOM Utilities */
+import global from "global"
 
 export function scrollForElement(element, direction = "vertical") {
   if (!element) return undefined
@@ -40,4 +41,94 @@ export function scrollElementToCenterOfParent(element, parent) {
   const elementTop = offsetTopRelativeTo(element, parent)
   const top = elementTop - parent.clientHeight / 2
   parent.scrollTo({ left: 0, top, behavior: "smooth" })
+}
+
+/**
+ * Return `styleDeclaration` for an `element`, or `{}` if there's an error accessing it.
+ *
+ * `element` can be an actual DOM element or a previously generated `computedStyle`.
+ * If you're getting lots of computed properties, get this once and pass to the routines below.
+ *
+ * NOTE: These objects may not be completely consistent cross-browser!
+ * See: https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle#Notes
+ */
+export function getComputedStyle(element) {
+  try {
+    if (element instanceof global.CSSStyleDeclaration) return element
+    return global.getComputedStyle(element)
+  } catch (e) {
+    return {}
+  }
+}
+
+/**
+ * Generic classes for efficiently working with T/L/B/R sizes for an `element`, based on `getComputedStyle`,
+ * e.g. for `padding` or margin.
+ */
+export class CSS_TLBR_VALUES {
+  #top
+  #right
+  #bottom
+  #left
+  constructor(propPrefix, elementOrStyle) {
+    const style = getComputedStyle(elementOrStyle)
+    this.#top = style[`${propPrefix}top`]
+    this.#right = style[`${propPrefix}right`]
+    this.#bottom = style[`${propPrefix}bottom`]
+    this.#left = style[`${propPrefix}left`]
+  }
+  get top() {
+    return parseFloat(this.#top, 10)
+  }
+  get right() {
+    return parseFloat(this.#right, 10)
+  }
+  get bottom() {
+    return parseFloat(this.#bottom, 10)
+  }
+  get left() {
+    return parseFloat(this.#left, 10)
+  }
+  get vertical() {
+    return this.top + this.bottom
+  }
+  get horizontal() {
+    return this.left + this.right
+  }
+}
+
+/**
+ * Return `margin` object for an `element` according to its `computedStyle`,
+ * as `{ top, right, bottom, left, vertical, horizontal }`.
+ *
+ * NOTE: `element` can be a DOM element or a previously obtained `styleDeclaration`.
+ * NOTE: Values will be `NaN` if you pass an invalid `element`.
+ */
+class margin extends CSS_TLBR_VALUES {}
+export function getMargin(element) {
+  return new margin("margin-", element)
+}
+
+/**
+ * Return `border` object for an `element` according to its `computedStyle`,
+ * as `{ top, right, bottom, left, vertical, horizontal }`.
+ *
+ * NOTE: `element` can be a DOM element or a previously obtained `styleDeclaration`.
+ * NOTE: Values will be `NaN` if you pass an invalid `element`.
+ */
+class border extends CSS_TLBR_VALUES {}
+export function getBorder(element) {
+  return new border("border-", element)
+}
+
+/**
+ * Return `padding` object for an `element` according to its `computedStyle`,
+ * as `{ top, right, bottom, left, vertical, horizontal }`.
+ *
+ * NOTE: `element` can be a DOM element or a previously obtained `styleDeclaration`.
+ * NOTE: Values will be `NaN` if you pass an invalid `element`.
+ */
+class padding extends CSS_TLBR_VALUES {}
+export function getPadding(element) {
+  return new padding("padding-", element)
 }
