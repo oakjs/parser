ae5838e9d781c0cea0bd18293a95e73068786eb9
Owen Williams <owenoak@gmail.com>
2025-06-11 21:19:37 -0400

    upgrade to react 18

--- src/languages/spell/ast/enzyme-setup.js
+++ src/languages/spell/ast/enzyme-setup.js
@@ -3,7 +3,8 @@
  * See `AST.renderedText`
  */
 import { configure, mount } from "enzyme"
-import Adapter from "enzyme-adapter-react-16"
+import Adapter from "@cfaester/enzyme-adapter-react-18"
+
 configure({ adapter: new Adapter() })
 
 export { mount }
